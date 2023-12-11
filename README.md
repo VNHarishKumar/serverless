57b76d5f4fc22cb0c34394504e0f9907f9c96fa1
VNHarishKumar <72001980+VNHarishKumar@users.noreply.github.com>
2023-12-10 21:33:33 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # serverless
+
+# Lambda Function setup
+
+Install the required npm packages
+
+- Google cloud storage
+- aws-sdk
+- axios 
+
+To run the Lambda function
+
+- Get the reuquired values from the IAC code as environment variables for Google cloud storage and amazon ses
+- Get the mailId for the mail need to be sent and the url to be stored in Google bucket as SNS message 
+- Split the message according to the information needed to be used
+- Check the url and store he content in the bucket
+- Send mail if able to save in bucket or unable to save in bucket
+- Track the mail sent in DynamoDb
---
+++ index.js
@@ -0,0 +1,289 @@
+
+'use strict';
+
+const { Storage } = require('@google-cloud/storage');
+const axios = require('axios');
+const { Buffer } = require('buffer');
+const AWS = require('aws-sdk');
+const mailgun = require("mailgun-js");
+const AdmZip = require('adm-zip');
+// const ses = new AWS.SES();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+// Decode the base64-encoded service account key
+const decodedKey = Buffer.from(process.env.saKey, 'base64').toString('utf-8');
+const serviceAccountKey = JSON.parse(decodedKey);
+
+// Create a Google Cloud Storage client with the service account key
+const storage = new Storage({
+  credentials: {
+    project_id:process.env.project_id,
+    client_email:process.env.acc_email,
+    private_key:serviceAccountKey.private_key,
+  },
+});
+
+AWS.config.update({ 
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey:process.env.secretAccessKey,
+  region: process.env.region,
+});
+const ses = new AWS.SES();
+
+
+module.exports.handler = async (event) => {
+
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('Google key',storage);
+
+  // Assuming 'event' is your JSON object
+  const message = event.Records[0].Sns.Message;
+  //  const [email, url] = message.split(',');
+  const [entriescount,allowed_attempts,deadline,email, url] = message.split(',');
+  console.log('Message:', message);
+  console.log('Attempt Number',entriescount);
+  console.log('Allowed attempts',allowed_attempts);
+  console.log('Deadline',deadline);
+  console.log('email',email);
+  console.log('url',url);
+
+
+    console.log("Before try block");
+    // ------------test mail gun------------------
+
+  //   const mg = mailgun({
+  //     apiKey: "",
+  //     domain: "demo.harishkumarvn.me",
+  // });
+
+  // const data = {
+  //     from: "harisshkumar10@gmail.com",
+  //     to: [email],
+  //     subject: "Hello",
+  //     text: "Testing Mailgun in AWS Lambda!",
+  // };
+
+  // mg.messages().send(data, (error, body) => {
+  //     if (error) {
+  //         console.error("Error sending email:", error);
+  //     } else {
+  //         console.log("Email sent successfully:", body);
+  //     }
+  // });
+
+    // -------------test end----------------------
+  try{
+    
+    const fileName = `${email}${Date.now()}.zip`;
+
+  // Define the bucket name and file name
+  // const bucketName = 'submission-bucket-002766063';
+  const bucketName = process.env.gcpBucketName;
+  const bucket = storage.bucket(bucketName);
+ 
+  
+  // Download the file from the URL
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+  const fileContent = Buffer.from(response.data);
+
+  // ---------------------testing file inside bucket-----------------------------------
+
+// Create an AdmZip instance with the downloaded buffer
+// const zip = new AdmZip(response.data);
+
+// // Get entries (files and directories) in the ZIP file
+// const entries = zip.getEntries();
+
+// // Check if there are any entries (files or directories)
+// if (entries && entries.length > 0) {
+//   console.log('ZIP file has contents:');
+//   entries.forEach((entry) => {
+//     console.log(entry.entryName);
+//   });
+// } else {
+//   console.log('ZIP file is empty.');
+// }
+
+// -------------------------Testing file inside bucket-------------------------
+
+    console.log('File entered try block');
+    console.log(fileContent);
+  
+  // if (fileContent.length > 0) {
+  const file = bucket.file(fileName);
+  console.log("before await function");
+  // await file.save(fileContent);
+  await storage.bucket(bucketName).file(fileName).save(fileContent);
+  console.log("after await function");
+
+  
+  console.log(`File from URL saved in Google Cloud Storage: gs://${bucketName}/${fileName}`);
+
+  
+
+// SES CODE in its own try-catch block
+
+  try {
+    console.log("Inside success mail try block");
+    // const sendemail = 'harishkumarvaithyannandhagopu@gmail.com';
+    const sendemail = email;
+    const emailParams = {
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: `
+          Dear User,
+
+          We like to inform you that the URL you provided is valid and processed. 
+          we reviewed the link and it is accurate. We were able to store your work and the attempt will be counted for submissions.
+          If you attempt and deadline remaining kindly try to submit again , other wise latest submission will be taken into account.
+          You cannot submmit if deadline and attempt is exceded.
+
+          Details:
+          - Attempts: ${entriescount}
+          - Allowed Attempts: ${allowed_attempts}
+          - Deadline for submission: ${deadline}
+          - File from URL saved in Google Cloud Storage: ${bucketName}/${fileName}
+          
+         
+          If you have any questions or concerns, please contact your professor or TA.
+
+          Sincerely,
+          Submission Team
+        `,
+          },
+        },
+        Subject: {
+          Data: 'Submission accepted',
+        },
+      },
+      Source: process.env.sourceEmail, // Replace with your SES verified email address
+      // Source: "harisshkumar10@harishkumarvn.me",
+    };
+
+     await ses.sendEmail(emailParams).promise();
+    console.log('Email sent successfully');
+
+    // var timestamp = Date.now();
+    var timestamp = new Date().toISOString();
+     // Log to DynamoDB
+     const dynamoParams = {
+      TableName: process.env.DYNAMO_TABLE_NAME, // Use the DynamoDB table name from environment variables
+      Item: {
+        EmailId: sendemail,
+        Timestamp: timestamp,
+        Status: "Success"
+        // Add additional attributes as needed
+      },
+    };
+     // Wrap DynamoDB operation in try-catch
+     try {
+      await dynamoDb.put(dynamoParams).promise();
+      console.log('Item added to DynamoDB successfully');
+    } catch (dynamoError) {
+      console.error('Error adding item to DynamoDB:', dynamoError);
+      // Handle DynamoDB error as needed
+    }
+
+
+    // await ses.sendEmail(emailParams).promise();
+    // console.log('Email sent successfully');
+  } catch (sesError) {
+    console.error('Error sending SES email:', sesError.message);
+    console.log('Email not sent due to an SES error');
+  }   }
+   catch (error) {
+    try {
+
+      console.log("Inside failure mail try block");
+
+    // const sendemail = 'harishkumarvaithyannandhagopu@gmail.com';
+    const sendemail = email;
+    const emailParams = {
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: `
+          Dear User,
+
+          We regret to inform you that the URL you provided is invalid and cannot be processed. 
+          Please review the link and ensure it is accurate. We were unable to store your work. But the attempt will be counted for submissions.
+          If you attempt and deadline remaining kindly try to submit again , other wise latest submission will be taken into account.
+          You cannot submmit if deadline and attempt is exceded.
+
+          Details:
+          - Attempts: ${entriescount}
+          - Allowed Attempts: ${allowed_attempts}
+          - Deadline for submission: ${deadline}
+          
+         
+          If you have any questions or concerns, please contact your professor or TA.
+
+          Sincerely,
+          Submission Team
+        `,
+          },
+        },
+        Subject: {
+          Data: 'Submission Rejected',
+        },
+      },
+      Source: process.env.sourceEmail, // Replace with your SES verified email address
+      // Source: "harisshkumar10@harishkumarvn.me",
+
+    };
+
+    var timestamp = new Date().toISOString();
+     // Log to DynamoDB
+     const dynamoParams = {
+      TableName: process.env.DYNAMO_TABLE_NAME, // Use the DynamoDB table name from environment variables
+      Item: {
+        EmailId: sendemail,
+        Timestamp: timestamp,
+        Status: "Failed"
+        // Add additional attributes as needed
+      },
+    };
+
+     await ses.sendEmail(emailParams).promise();
+    console.log('Email sent successfully');
+
+     // Wrap DynamoDB operation in try-catch
+     try {
+      await dynamoDb.put(dynamoParams).promise();
+      console.log('Item added to DynamoDB successfully');
+    } catch (dynamoError) {
+      console.error('Error adding item to DynamoDB:', dynamoError);
+      // Handle DynamoDB error as needed
+    }
+
+    // await ses.sendEmail(emailParams).promise();
+    // console.log('Invalid Email sent successfully');
+  } catch (sesError) {
+    console.error('Error sending SES email:', sesError.message);
+    console.log('Email not sent due to an SES error');
+  }
+      console.error('Error downloading and saving the file:', error.message);
+    }
+ 
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+
+
